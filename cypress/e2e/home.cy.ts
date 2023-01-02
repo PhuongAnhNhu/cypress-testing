c1abcce89d3fcea8b05a88b8bf4341196d5cefcf
Anna Phuong Anh <phuong.nhu@endava.com>
2023-01-02 14:49:25 +0100

    test a array string

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -9,7 +9,10 @@ describe("home page", () => {
       .contains("Testing Next.js Applications with Cypress")
   })
   // it.only : just run those specific test, which have only
-  it.only("the features on the homepage are correct", () => {
+  it("the features on the homepage are correct", () => {
     cy.get("dt").eq(0).contains("4 Courses")
+    cy.get("dt").eq(1).contains("25+ Lessons")
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
+ 
 })
