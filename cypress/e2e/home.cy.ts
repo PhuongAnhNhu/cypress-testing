f9a12eff270d53126fd68d1a2d9ad61aa14d8032
Anna Phuong Anh <phuong.nhu@endava.com>
2023-01-02 14:43:56 +0100

    used beforeEach

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,7 +1,15 @@
-describe('home page', () => {
-  it('the h1 contains the correct text', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get("h1").contains("Testing Next.js Applications with Cypress")
+describe("home page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/")
+  })
+  it("the h1 contains the correct text", () => {
+    // using data specific test attributes on elements
+    cy.get("[data-test='hero-heading']")
+      .should("exist")
+      .contains("Testing Next.js Applications with Cypress")
+  })
+  // it.only : just run those specific test, which have only
+  it.only("the features on the homepage are correct", () => {
+    cy.get("dt").eq(0).contains("4 Courses")
   })
 })
-
