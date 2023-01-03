8a525f94cabad9ecdb1a1e0462f0c4b82c95051d
Anna Phuong Anh <phuong.nhu@endava.com>
2023-01-03 09:45:05 +0100

    test invalid email and does email already exis

--- cypress/e2e/subcribe.cy.ts
+++ cypress/e2e/subcribe.cy.ts
@@ -8,5 +8,17 @@ describe("New Letter Subcribe Form", () => {
     cy.getByData('submit-button').click()
     cy.getByData('success-message').should('exist').contains(email)
   })
- 
+  it("does not allow invalid email adress", () => {
+    const email = 'nhuphuonganh.com';
+    cy.getByData('email-input').type(email)
+    cy.getByData('submit-button').click()
+    cy.getByData('success-message').should('not.exist')
+  })
+
+  it("email is already exists", () => {
+    const email = 'john@example.com';
+    cy.getByData('email-input').type(email)
+    cy.getByData('submit-button').click()
+    cy.getByData('server-error-message').should('exist').contains('already exists. Please use a different email address.');
+  })
 })
